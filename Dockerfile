02a658095665e141a19f6af35a161ab0624e2cc4
David Tang <dtang@egnyte.com>
2019-04-25 16:01:00 -0700

    Add dataset and tokenize lines

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.7.3-slim-stretch
 
 COPY requirements.txt /
 RUN pip install --no-cache-dir -r /requirements.txt
-COPY project.py /opt/project/
+COPY . /opt/project/
 
 ENTRYPOINT ["/opt/project/project.py"]
+#ENTRYPOINT ["sleep", "infinity"]
--- project.py
+++ project.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
 
+def parse_data(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    lines = list(map(lambda x: x.strip().split(','), lines))
+    return lines
+
+
 if __name__ == '__main__':
     print('Running project')
+    lines = parse_data('/opt/project/dataset/kddcup.data_10_percent')
+    print(lines[:2])
