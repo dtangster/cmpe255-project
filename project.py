de573a997c73dc8b40c0f8b7fd5a7d9a7dcc3ea0
David Tang <dtang@egnyte.com>
2019-05-06 12:47:58 -0700

    Create mapping of different attacks to a category

--- project.py
+++ project.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python
+
 import keras
 import numpy
 import pandas
 import scipy
 import tensorflow
+
+
+def parse_attack_types(filename):
+    attack_map = {}
+    with open(filename) as f:
+        lines = f.readlines()
+    for line in lines:
+        attack, attack_type = line.split()
+        if attack_type in attack_map:
+            attack_map[attack_type].add(attack)
+        else:
+            attack_map[attack_type] = {attack}
+    return attack_map
 
 
 def parse_data(filename):
@@ -14,5 +29,7 @@ def parse_data(filename):
 
 if __name__ == '__main__':
     print('Running project')
-    lines = parse_data('/opt/project/dataset/kddcup.data_10_percent')
+    attack_map = parse_attack_types('./dataset/attack_types.txt')
+    print(attack_map)
+    lines = parse_data('./dataset/kddcup.data_10_percent')
     print(lines[:2])
